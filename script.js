bd6f0f792f9ec74e7dbec2f3cf7df7acf959369c
zmining <70307777+Zmining@users.noreply.github.com>
2024-08-21 15:21:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ fetch('https://api.github.com/users/zmining/repos', {
     if (Array.isArray(data)) {
         data.filter(repo => !projetosParaExcluir.includes(repo.name))
             .forEach(repo => {
-            const projetoElement = document.createElement('div');
-            projetoElement.className = 'projeto';
-            projetoElement.innerHTML = `
-                <h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>
-                <p>${repo.description || 'Sem descrição'}</p>
-            `;
-            projetosContainer.appendChild(projetoElement);
-        });
+                const projetoElement = document.createElement('div');
+                projetoElement.className = 'projeto';
+                projetoElement.innerHTML = `
+                    <h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>
+                    <p>${repo.description || 'Sem descrição'}</p>
+                `;
+                projetosContainer.appendChild(projetoElement);
             });
     } else {
         console.error('Resposta inesperada:', data);
@@ -24,12 +23,11 @@ fetch('https://api.github.com/users/zmining/repos', {
 })
 .catch(error => console.error('Erro ao carregar repositórios:', error));
 
-
 const curriculum = document.getElementById('curriculum');
 
 curriculum.addEventListener('click', function(){
     const link = document.createElement('a')
     link.href = './assets/archives/resume.pdf'
     link.download = 'resume.pdf'
-    link.click()
-})
+    link.click();
+});
